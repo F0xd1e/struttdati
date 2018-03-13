3f6b6a799a37955d99ab7d1a12181080dc70227a
Luis <bobo121@hotmail.it>
2018-03-13 12:43:23 +0100

    Add files via upload

---
+++ vector.h
@@ -0,0 +1,4 @@
+int arr_add(int *arr, int size, int rie); //somma elementi di un'array
+int arr_add_pos(int *arr, int *arr2, int *arroutput); // somma di due array e output in terzo array
+int scal_arr(int *arr, int *arr2); // prodotto scalare di due array
+
